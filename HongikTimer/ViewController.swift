084c136dc07836f2ffeeb2b3fb4c52f3ce5d9f2f
seaLion3 <qnm541@gmail.com>
2022-09-13 12:09:32 +0900

    Initial Commit

---
+++ HongikTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HongikTimer
+//
+//  Created by JongHoon on 2022/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
