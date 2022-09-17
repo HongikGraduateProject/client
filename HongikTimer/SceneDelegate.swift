7c5e782333892fa6bd493334cdc1fb379164a56d
seaLion3 <qnm541@gmail.com>
2022-09-18 03:38:09 +0900

    kakao 로그인 에러, SceneDelegate 수정

--- HongikTimer/SceneDelegate.swift
+++ HongikTimer/SceneDelegate.swift
@@ -34,18 +34,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-//        if let url = URLContexts.first?.url {
-//            if AuthApi.isKakaoTalkLoginUrl(url) {
-//                _ = AuthController.handleOpenUrl(url: url)
-//            } else {
-//                NaverThirdPartyLoginConnection
-//                   .getSharedInstance()?
-//                   .receiveAccessToken(URLContexts.first?.url)
-//            }
-//        }
-        
-        NaverThirdPartyLoginConnection
-           .getSharedInstance()?
-           .receiveAccessToken(URLContexts.first?.url)
+        if let url = URLContexts.first?.url {
+            if AuthApi.isKakaoTalkLoginUrl(url) {
+                _ = AuthController.handleOpenUrl(url: url)
+            } else {
+                NaverThirdPartyLoginConnection
+                    .getSharedInstance()?
+                    .receiveAccessToken(URLContexts.first?.url)
+            }
+        }
     }
 }
